4f0edeac42bcec927bc10567e28e4dae8bd0f223
Jason Chang <jasonchang216@gmail.com>
2020-04-08 16:47:12 -0400

    Add constraints and add exercises
    Added UI constraints and added exercise arrays. Basic MVP of randomly
    choosing workouts is complete.

--- stax/ViewController.swift
+++ stax/ViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var exerciseLabel: UILabel!
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
     
-    let history = [Int]
+    let countList: [String] = ["20", "10", "15", "30", "10", "5", "10", "20", "10", "30"]
+    let exerciseList: [String] = ["SQUATS", "PUSHUPS", "SECONDS OF PLANK", "JUMPJACKS", "LUNGES (EACH LEG)", "BURPEES", "UP-DOWN PLANKS", "LATERAL LUNGES (EACH SIDE)", "V-UPS", "SECONDS OF HIGH KNEES"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainTitle.text = "TIME TO WORKOUT"
+        countLabel.text = " "
+        exerciseLabel.text = " "
+        
+        collectionView.collectionViewLayout = configureLayout()
+        
     }
     
-    
-    @IBAction func button(_ sender: Any) {
-        let number = Int.random(in: 0 ... 10)
-        label.text = "Random number is \(number)"
+    @IBAction func nextExercise(_ sender: Any) {
+        let number = Int.random(in: 0 ... countList.count - 1)
+        mainTitle.text = "DO"
+        countLabel.text = "\(countList[number])"
+        exerciseLabel.text = "\(exerciseList[number])"
+        (sender as! UIButton).setTitle("NEXT", for: [])
     }
     
 
