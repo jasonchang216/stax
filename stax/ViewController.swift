1475c8e3943515cedb73a141a7565d964534926f
Jason Chang <jasonchang216@gmail.com>
2020-04-08 20:34:40 -0400

    Added timer
    Added timer function with reset button

--- stax/ViewController.swift
+++ stax/ViewController.swift
@@ -9,25 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var countLabel: UILabel!
-    @IBOutlet weak var exerciseLabel: UILabel!
-    @IBOutlet weak var mainTitle: UILabel!
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    let countList: [String] = ["20", "10", "15", "30", "10", "5", "10", "20", "10", "30"]
-    let exerciseList: [String] = ["SQUATS", "PUSHUPS", "SECONDS OF PLANK", "JUMPJACKS", "LUNGES (EACH LEG)", "BURPEES", "UP-DOWN PLANKS", "LATERAL LUNGES (EACH SIDE)", "V-UPS", "SECONDS OF HIGH KNEES"]
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         mainTitle.text = "TIME TO WORKOUT"
         countLabel.text = " "
         exerciseLabel.text = " "
-        
-        collectionView.collectionViewLayout = configureLayout()
-        
     }
+    
+//Exercise randomizer
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var exerciseLabel: UILabel!
+    @IBOutlet weak var mainTitle: UILabel!
+    
+    let countList: [String] = ["20", "10", "15", "30", "10", "5", "10", "20", "10", "30"]
+    let exerciseList: [String] = ["SQUATS", "PUSHUPS", "SECONDS OF PLANK", "JUMPJACKS", "LUNGES (EACH LEG)", "BURPEES", "UP-DOWN PLANKS", "LATERAL LUNGES (EACH SIDE)", "V-UPS", "SECONDS OF HIGH KNEES"]
     
     @IBAction func nextExercise(_ sender: Any) {
         let number = Int.random(in: 0 ... countList.count - 1)
@@ -35,8 +32,53 @@ class ViewController: UIViewController {
         countLabel.text = "\(countList[number])"
         exerciseLabel.text = "\(exerciseList[number])"
         (sender as! UIButton).setTitle("NEXT", for: [])
+        if isTimerRunning == false {
+            runTimer()
+        }
     }
     
-
+//Time keeper
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    func timeString(time: TimeInterval) -> String {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
+    
+    @objc func updateTimer() {
+        seconds -= 1
+        timerLabel.text = timeString(time: TimeInterval(seconds))
+        
+        if seconds < 1 {
+            timer.invalidate()
+            mainTitle.text = "TIME TO WORKOUT"
+            countLabel.text = " "
+            exerciseLabel.text = " "
+            isTimerRunning = false
+            seconds = 60
+            
+            let alert = UIAlertController(title: "WORKOUT COMPLETE", message: "YOU'RE DONE.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)),userInfo: nil, repeats: true)
+        isTimerRunning = true
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        timer.invalidate()
+        seconds = 60
+        timerLabel.text = timeString(time: TimeInterval(seconds))
+        isTimerRunning = false
+    }
+    
 }
 
